7a7b987aad8712cc0c31ce2399ee9d7a7ecbdb8c
novakvova <novakvova@gmail.com>
2019-06-27 16:14:46 +0300

    Croperis good

--- src/components/animal/addcropper/index.js
+++ src/components/animal/addcropper/index.js
@@ -12,25 +12,24 @@ class AnimalAddCropperWidgetContainer extends Component {
         super(props);
         this.state = {
             name: '',
-            image: '',
+            image: null,
             src: '',
             isCropped: false,
-            cropResult: null
         };
     }
     onSubmitForm = (e) => {
         e.preventDefault();
         console.log('----submit form---');
 
-        const model = { 
+        const model = {
             name: this.state.name,
-            image: this.state.image 
+            image: this.state.image
         };
-        axios.post('http://localhost:100/api/gallery.php', model)
+        axios.post('https://localhost:44378/api/animal/add-base64', model)
             .then(
-                (resp)=>{
+                (resp) => {
                     console.log('--success post--', resp.data);
-                    this.props.history.push('/gallery');
+                    this.props.history.push('/animal');
                 },
                 (err) => {
                     console.log('--err problem---', err);
@@ -39,12 +38,12 @@ class AnimalAddCropperWidgetContainer extends Component {
     }
 
     onChangeInput = (e) => {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({ [e.target.name]: e.target.value });
         console.log(e.target.name, e.target.value);
 
     }
 
-    onSelectImage = () =>{
+    onSelectImage = () => {
         console.log('-------Upload image--------');
         this.inputFileElement.click();
     }
@@ -61,7 +60,7 @@ class AnimalAddCropperWidgetContainer extends Component {
                 const reader = new FileReader();
                 reader.onload = () => {
                     this.setState({ src: reader.result, isCropped: true });
-                    
+
                 };
                 reader.readAsDataURL(files[0]);
             }
@@ -82,85 +81,78 @@ class AnimalAddCropperWidgetContainer extends Component {
     cropImage = (e) => {
         e.preventDefault();
         if (typeof this.cropper.getCroppedCanvas() === 'undefined') {
-          return;
+            return;
         }
         this.setState({
-          cropResult: this.cropper.getCroppedCanvas().toDataURL(),
-          isCropped: false 
+            image: this.cropper.getCroppedCanvas().toDataURL(),
+            isCropped: false
         });
-      }
+    }
 
-    render() { 
-        const {name, image, src, isCropped, cropResult }= this.state;
-        
-        return ( 
+    render() {
+        const { name, image, src, isCropped } = this.state;
+        const imgDef = image !== null ? image : 'https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg';
+        return (
             <React.Fragment>
-                <h1>Додати фото в галерею кропер</h1> 
+                <h1>Додати фото в галерею кропер</h1>
                 <form onSubmit={this.onSubmitForm}>
                     <div className="form-group">
                         <label htmlFor="name">Назва тварини:</label>
-                        <input type="text" 
-                                className="form-control" 
-                                name="name" 
-                                id="name"
-                                onChange={this.onChangeInput}
-                                value={name} />
+                        <input type="text"
+                            className="form-control"
+                            name="name"
+                            id="name"
+                            onChange={this.onChangeInput}
+                            value={name} />
                     </div>
 
                     <div className="form-group" >
-                        <img className="imgUpload" 
-                            onClick={this.onSelectImage} 
-                            src="https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg" />
+                        <img className="imgUpload"
+                            onClick={this.onSelectImage}
+                            src={imgDef} />
 
-                        <input ref={input => this.inputFileElement = input} 
-                                type="file" 
-                                onChange={this.onChangeSelecFile} 
-                                className="d-none" />
+                        <input ref={input => this.inputFileElement = input}
+                            type="file"
+                            onChange={this.onChangeSelecFile}
+                            className="d-none" />
                     </div>
                     <button type="submit" className="btn btn-info">Додати</button>
                 </form>
 
-                <img src={cropResult} />
-
                 <div className={classnames('croppermodal', { 'open': isCropped })}>
-                <div className="container">
-                    <div className="row" style={{ marginTop: '100px' }}>
-                        <div className="col-md-8">
-                            <Cropper
-                                style={{ height: 400, width: '100%' }}
-                                aspectRatio={1 / 1}
-                                guides={false}
-                                viewMode={1}
-                                preview=".img-preview"
-                                src={src}   
-                                ref={cropper => { this.cropper = cropper; }}
-                            />
-                            
-                        </div>
-                        <div className="col-md-4">
-                            
-                            <div>
-                                <div className="box" style={{ width: '100%' }}>
-                                
-                                    <button className="btn btn-success" onClick={this.cropImage}>Обрізати фото</button>
-                                    <button className="btn btn-danger" onClick={this.onCloseCropper}>Закрити</button>
-                                    <br />
-                                    <div  style={{ marginTop: 20 }}>
-                                        <div className="img-preview" style={{ height: 300 }} />
-                                    </div>
-                                </div>
+                    <div className="container">
+                        <div className="row" style={{ marginTop: '100px' }}>
+                            <div className="col-md-8">
+                                <Cropper
+                                    style={{ height: 400, width: '100%' }}
+                                    aspectRatio={1 / 1}
+                                    guides={false}
+                                    viewMode={1}
+                                    preview=".img-preview"
+                                    src={src}
+                                    ref={cropper => { this.cropper = cropper; }}
+                                />
 
                             </div>
+                            <div className="col-md-4">
+                                <div>
+                                    <div className="box" style={{ width: '100%' }}>
+                                        <button className="btn btn-success" onClick={this.cropImage}>Обрізати фото</button>
+                                        <button className="btn btn-danger" onClick={this.onCloseCropper}>Закрити</button>
+                                        <br />
+                                        <div style={{ marginTop: 20 }}>
+                                            <div className="img-preview" style={{ height: 300 }} />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        
-
                     </div>
                 </div>
-            </div>
             </React.Fragment>
         );
     }
 }
- 
+
 const AnimalAddCropperWidget = AnimalAddCropperWidgetContainer;
 export default AnimalAddCropperWidget;
